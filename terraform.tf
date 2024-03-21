0af247e16563ae590fa9accedd4af04841f86135
Deepika <deepikapeelam16@gmail.com>
2024-03-21 12:47:42 +0530

    Terraform

--- terraform.tf
+++ terraform.tf
@@ -1,4 +1,9 @@
-resource "aws_instance" "ec2" {
-  ami                     = "ami-03035978b5aeb1274"
-  instance_type           = "t2.micro"
+provider "aws" {
+  region = "eu-north-1" # specify your desired region
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-03035978b5aeb1274"
+  instance_type = "t2.micro"
+  key_name      = "WorkStation.pem"
 }
